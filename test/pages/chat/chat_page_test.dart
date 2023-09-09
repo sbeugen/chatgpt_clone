5bc66f730aa8dfa58059661e66b8d410a592d95b
Eugen Sawitzki <e.sawitzki@reply.de>
2023-09-09 16:43:34 +0200

    add unit tests for no api key set modal

---
+++ test/pages/chat/chat_page_test.dart
@@ -0,0 +1,52 @@
+import 'package:chatgpt_clone/pages/chat/chat_page.dart';
+import 'package:chatgpt_clone/providers/chatgpt/chatgpt_model.dart';
+import 'package:chatgpt_clone/providers/settings/settings_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+
+import 'chat_page_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<ChatGPTModel>(), MockSpec<SettingsModel>()])
+void main() {
+  group('missing api key dialog', () {
+    testWidgets('should appear if api key is not set', (tester) async {
+      final mockChatGPTModel = MockChatGPTModel();
+
+      final mockSettingsModel = MockSettingsModel();
+      when(mockSettingsModel.initialized).thenReturn(true);
+
+      await tester
+          .pumpWidget(createWidget(mockSettingsModel, mockChatGPTModel));
+      await tester.pumpAndSettle();
+
+      expect(find.text('No OpenAI API key set'), findsOneWidget);
+    });
+
+    testWidgets('should not appear if api key is set/client is initialized', (tester) async {
+      final mockChatGPTModel = MockChatGPTModel();
+      when(mockChatGPTModel.clientInitialized).thenReturn(true);
+
+      final mockSettingsModel = MockSettingsModel();
+      when(mockSettingsModel.initialized).thenReturn(true);
+
+      await tester
+          .pumpWidget(createWidget(mockSettingsModel, mockChatGPTModel));
+      await tester.pumpAndSettle();
+
+      expect(find.text('No OpenAI API key set'), findsNothing);
+    });
+  });
+}
+
+Widget createWidget(SettingsModel settingsModel, ChatGPTModel chatGPTModel) =>
+    MaterialApp(
+        home: MultiProvider(
+      providers: [
+        ChangeNotifierProvider<SettingsModel>(create: (_) => settingsModel),
+        ChangeNotifierProvider<ChatGPTModel>(create: (_) => chatGPTModel),
+      ],
+      child: const ChatPage(),
+    ));
